625d9f8f85b298589bd04901f7f7c25916523c14
pyk <2316890929@qq.com>
2026-08-14 22:16:52 +0800

    新增了lsh_launch.c文件

---
+++ lsh_launch.c
@@ -0,0 +1,28 @@
+int lsh_launch(char **args)
+{
+	pid_t pid,wpid;//process id type
+	//进程id类型的变量
+	int status;
+
+	pid = fork();//创建一个子进程
+	if(pid==0)//如果当前的进程是子进程
+	{
+		if(execvp(args[0],args)==-1)//执行另一个程序
+		{
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	}else if(pid<0)//如果子进程创建失败
+	{
+		perror("lsh");
+	}else
+	{
+		do
+		{
+			wpid=waitpid(pid,&status,WUNTRACED);//等待未被追踪但被暂停的子进程
+		}while(!WIFEXITED(status)&&!WIFSIGNALED(status));
+		//Wait If Exited,子进程是否正常退出了？
+		//Wait If Signaled，子进程是否被信号终止了？
+	}
+	return 1;//1代表shell可以继续运行
+}
